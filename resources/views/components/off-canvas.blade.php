48ea018876aad334bd2fc9de302bd4400f6797b8
Elisha Ukpong <ishukpong418@gmail.com>
2024-07-08 18:12:09 +0100

    wrapped up sidebar

--- resources/views/components/off-canvas.blade.php
+++ resources/views/components/off-canvas.blade.php
@@ -43,30 +43,41 @@
             </div>
 
             <div class="flex flex-shrink-0 items-center px-4">
-                <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+                <x-application-logo />
             </div>
-            <div class="mt-5 h-0 flex-1 overflow-y-auto">
+            <div class="mt-5 h-0 flex-1 overflow-y-auto p-4">
                 <nav class="space-y-1 px-2">
-                    @foreach($menu as $item)
-                        <a href="{{ data_get($item, 'path') }}"
-                            @class([
-                                 'group flex items-center px-2 py-2 text-base font-medium rounded-md',
-                                 'bg-gray-100 text-gray-900' => request()->is(trim(data_get($item, 'path'), '/')),
-                                 'text-gray-600 hover:bg-gray-50 hover:text-gray-900' => !request()->is(trim(data_get($item, 'path'), '/')),
-                             ])>
-                            @if(data_get($item, 'icon'))
-                                <span @class([
-                                        'mr-4 flex-shrink-0 h-6 w-6',
-                                        'text-gray-500' => request()->is(trim(data_get($item, 'path'), '/')),
-                                        'text-gray-400 group-hover:text-gray-500' => !request()->is(trim(data_get($item, 'path'), '/')),
-                                    ])>
-                                        {!! data_get($item, 'icon') !!}
-                                    </span>
-                            @endif
-                            {{ data_get($item, 'name') }}
-                        </a>
+                    @foreach($menu as $heading => $items)
+                        <div class="pb-12">
+                            <x-navigation-header :title="$heading"/>
+
+                            <div>
+                                @foreach($items as $item)
+                                    <x-navigation-link-item :item="$item" />
+                                @endforeach
+                            </div>
+                        </div>
                     @endforeach
                 </nav>
+
+                <div class="flex items-end justify-between mb-8">
+
+                    <div class="flex">
+                        <img src="{{ asset('img/user.png') }}" alt="">
+
+                        <div class="ml-2">
+                            <p class="text-md font-bold">{{ auth()->user()->name }}</p>
+                            <p class="text-sm font-light">{{ auth()->user()->email }}</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5.25008 2.33334C5.71032 2.33334 6.08342 1.96024 6.08342 1.50001C6.08342 1.03977 5.71032 0.666672 5.25008 0.666672H4.00008C2.15913 0.666672 0.666748 2.15906 0.666748 4.00001V14C0.666748 15.841 2.15913 17.3333 4.00008 17.3333H5.25008C5.71032 17.3333 6.08341 16.9602 6.08341 16.5C6.08341 16.0398 5.71032 15.6667 5.25008 15.6667H4.00008C3.07961 15.6667 2.33341 14.9205 2.33341 14L2.33342 4.00001C2.33342 3.07953 3.07961 2.33334 4.00008 2.33334H5.25008Z" fill="black"/>
+                            <path d="M17.9227 9.58926C18.2481 9.26382 18.2481 8.73619 17.9227 8.41075L14.5893 5.07742C14.2639 4.75198 13.7363 4.75198 13.4108 5.07742C13.0854 5.40285 13.0854 5.93049 13.4108 6.25593L15.3216 8.16667L5.66675 8.16667C5.20651 8.16667 4.83341 8.53977 4.83341 9C4.83341 9.46024 5.20651 9.83334 5.66675 9.83334L15.3216 9.83334L13.4108 11.7441C13.0854 12.0695 13.0854 12.5972 13.4108 12.9226C13.7363 13.248 14.2639 13.248 14.5893 12.9226L17.9227 9.58926Z" fill="black"/>
+                        </svg>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
