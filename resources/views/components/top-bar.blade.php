ebec83e0f573f522e28971443735c34d5f6d401d
Elisha Ukpong <ishukpong418@gmail.com>
2024-07-08 13:46:51 +0100

    switched to jetstream

---
+++ resources/views/components/top-bar.blade.php
@@ -0,0 +1,68 @@
+<div class="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white shadow">
+    <button
+            x-on:click="sbOpen = true"
+            type="button" class="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden">
+        <span class="sr-only">{{ __('Open sidebar') }}</span>
+        <!-- Heroicon name: outline/bars-3-bottom-left -->
+        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+        </svg>
+    </button>
+    <div class="flex flex-1 justify-between px-4">
+        <div class="flex flex-1">
+            <form class="flex w-full md:ml-0" action="#" method="GET">
+                <label for="search-field" class="sr-only">{{ __('Search') }}</label>
+                <div class="relative w-full text-gray-400 focus-within:text-gray-600">
+                    <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center">
+                        <!-- Heroicon name: mini/magnifying-glass -->
+                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <input id="search-field" class="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-sm" placeholder="Search" type="search" name="search">
+                </div>
+            </form>
+        </div>
+        <div class="ml-4 flex items-center md:ml-6" x-data="{open: false}">
+
+            <!-- Profile dropdown -->
+            <div class="relative ml-3">
+                <div>
+                    <button
+                            x-on:click="open=!open"
+                            type="button" class="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                        <span class="sr-only">Open user menu</span>
+                            <span class="text-primary-400">
+                                <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            </span>
+                    </button>
+                </div>
+
+                <div
+                        x-cloak
+                        x-show="open"
+                        x-on:click.away="open=false"
+                        x-transition:enter="transition ease-out duration-100"
+                        x-transition:enter-start="transform opacity-0 scale-95"
+                        x-transition:enter-end="transform opacity-100 scale-100"
+                        x-transition:leave="transition ease-in duration-75"
+                        x-transition:leave-start="transform opacity-100 scale-100"
+                        x-transition:leave-end="transform opacity-0 scale-95"
+                        class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+
+                    <form method="POST" action="{{ route('logout') }}" x-data>
+                        @csrf
+
+                        <a role="menuitem"
+                           href="#"
+                           x-on:click.prevent="$root.submit();"
+                                @class([
+                                    'block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100',
+                                    'bg-gray-100' => request()->routeIs('api-tokens.index')
+                                ])>{{ __('Log Out') }}</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,24 +13,33 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Styles -->
+        @livewireStyles
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+        <x-banner />
+
+        <div class="min-h-screen bg-gray-100">
+            @livewire('navigation-menu')
 
             <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
+            @if (isset($header))
+                <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endisset
+            @endif
 
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
         </div>
+
+        @stack('modals')
+
+        @livewireScripts
     </body>
 </html>
