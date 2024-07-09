481d1e250a769ae670587926f5180d488f6cbbbe
Elisha Ukpong <ishukpong418@gmail.com>
2024-07-09 21:15:18 +0100

    added few more changes

--- resources/views/dashboard/compliance.blade.php
+++ resources/views/dashboard/compliance.blade.php
@@ -5,21 +5,21 @@
                 Business Profile
             </x-compliance-sidebox>
 
-            <x-compliance-sidebox :active="true">
+            <x-compliance-sidebox :active="false">
                 Contact
             </x-compliance-sidebox>
 
-            <x-compliance-sidebox>
+            <x-compliance-sidebox :active="false">
                 Owner
             </x-compliance-sidebox>
 
-            <x-compliance-sidebox>
+            <x-compliance-sidebox :active="true">
                 Account
             </x-compliance-sidebox>
 
         </div>
 
-        <div class="w-2/3" x-data="{showBusiness:false}">
+        <div class="w-2/3" x-data="{showBusiness:false, showContact:false, showOwner:false}">
             <div x-cloak x-show="showBusiness" class="bg-white flex p-8 flex-col rounded-[10px]">
                 <div class="pb-6">
                     <p class="font-medium text-base text-black">Business Profile</p>
@@ -81,18 +81,127 @@
                 </div>
             </div>
 
-            <div class="bg-white flex p-8 flex-col rounded-[10px] mb-8">
-                <div class="pb-6">
-                    <p class="font-medium text-base text-black">Contact</p>
+            <div x-cloak x-show="showContact">
+                <div class="bg-white flex p-8 flex-col rounded-[10px] mb-8">
+                    <div class="pb-6">
+                        <p class="font-medium text-base text-black">Contact</p>
+                    </div>
+
+                    <div>
+                        <div class="pb-4">
+                            <x-input-label class="pb-2">
+                                General Email
+                            </x-input-label>
+
+                            <x-input class="w-full p-4 placeholder-gray-300" placeholder="Enter Email Address"/>
+                        </div>
+
+                        <div class="pb-4">
+                            <x-input-label class="pb-2">
+                                Phone Number
+                            </x-input-label>
+
+                            <x-input class="w-full p-4 placeholder-gray-300" placeholder="Enter Phone Number"/>
+                        </div>
+
+                        <div>
+                            <a class="cursor-pointer flex">
+                                <x-svg.plus/>
+                                <span class="text-primary-pink-500 text-sm"> Add Social Links</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="bg-white flex p-8 flex-col rounded-[10px]">
+                    <div class="pb-6">
+                        <p class="font-medium text-base text-black">Office Address</p>
+                    </div>
+
+                    <div>
+                        <div class="pb-4">
+                            <x-input-label class="pb-2">
+                                Country
+                            </x-input-label>
+
+                            <button class="flex text-gray-800 px-4 py-1 bg-nav-gray-50 rounded-full text-[14px] items-center">
+                                <x-svg.nigeria/>
+                                <span class="ml-2">
+                                Nigeria
+                            </span>
+                            </button>
+                        </div>
+
+                        <div class="pb-4">
+                            <x-input-label class="pb-2">
+                                State
+                            </x-input-label>
+
+                            <x-select class="w-full p-4 placeholder-gray-300"/>
+                        </div>
+
+                        <div class="pb-4">
+                            <x-input-label class="pb-2">
+                                City
+                            </x-input-label>
+
+                            <x-select class="w-full p-4 placeholder-gray-300"/>
+                        </div>
+
+                        <div class="pb-4">
+                            <x-input-label class="pb-2">
+                                Street Address
+                            </x-input-label>
+
+                            <x-text-input class="w-full p-4 placeholder-gray-300" placeholder="Enter Business Name"/>
+                        </div>
+
+                        <div class="pb-6">
+                            <x-input-label class="pb-2">
+                                Complex or Building Number (Optional)
+                            </x-input-label>
+
+                            <x-text-input class="w-full p-4 placeholder-gray-300" placeholder="Complex or Building Number "/>
+                        </div>
+
+                        <div>
+                            <button type="submit" class="text-white bg-primary-pink-500 rounded-md py-2 px-4 w-full">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div x-cloak x-show="showOwner" class="bg-white flex p-8 flex-col rounded-[10px] mb-8">
+                <div class="pb-6 flex justify-between">
+                    <p class="font-medium text-base text-black">Owner's Profile</p>
+
+                    <button class="flex text-gray-800 px-4 py-1 bg-nav-gray-50 rounded-full text-[14px] items-center">
+                        <x-svg.nigeria/>
+                        <span class="ml-2">
+                            Nigeria
+                        </span>
+                    </button>
+
                 </div>
 
                 <div>
-                    <div class="pb-4">
-                        <x-input-label class="pb-2">
-                            General Email
-                        </x-input-label>
+                    <div class="pb-4 flex flex-col md:flex-row">
+                        <div class="md:w-1/2 md:mr-4">
+                            <x-input-label class="pb-2">
+                                First Name
+                            </x-input-label>
 
-                        <x-input class="w-full p-4 placeholder-gray-300" placeholder="Enter Email Address"/>
+                            <x-input class="w-full p-4 placeholder-gray-300" placeholder="Enter Email Address"/>
+                        </div>
+
+                        <div class="md:w-1/2">
+                            <x-input-label class="pb-2">
+                                Last Name
+                            </x-input-label>
+
+                            <x-input class="w-full p-4 placeholder-gray-300" placeholder="Enter Email Address"/>
+                        </div>
+
                     </div>
 
                     <div class="pb-4">
@@ -103,64 +212,12 @@
                         <x-input class="w-full p-4 placeholder-gray-300" placeholder="Enter Phone Number"/>
                     </div>
 
-                    <div>
-                        <a class="cursor-pointer flex">
-                            <x-svg.plus/>
-                            <span class="text-primary-pink-500 text-sm"> Add Social Links</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="bg-white flex p-8 flex-col rounded-[10px]">
-                <div class="pb-6">
-                    <p class="font-medium text-base text-black">Office Address</p>
-                </div>
-
-                <div>
-                    <div class="pb-4">
-                        <x-input-label class="pb-2">
-                            Country
-                        </x-input-label>
-
-                        <button class="flex text-gray-800 px-4 py-1 bg-nav-gray-50 rounded-full text-[14px] items-center">
-                            <x-svg.nigeria/>
-                            <span class="ml-2">
-                                Nigeria
-                            </span>
-                        </button>
-                    </div>
-
-                    <div class="pb-4">
-                        <x-input-label class="pb-2">
-                            State
-                        </x-input-label>
-
-                        <x-select class="w-full p-4 placeholder-gray-300"/>
-                    </div>
-
-                    <div class="pb-4">
-                        <x-input-label class="pb-2">
-                            City
-                        </x-input-label>
-
-                        <x-select class="w-full p-4 placeholder-gray-300"/>
-                    </div>
-
-                    <div class="pb-4">
-                        <x-input-label class="pb-2">
-                            Street Address
-                        </x-input-label>
-
-                        <x-text-input class="w-full p-4 placeholder-gray-300" placeholder="Enter Business Name"/>
-                    </div>
-
                     <div class="pb-6">
                         <x-input-label class="pb-2">
-                            Complex or Building Number (Optional)
+                            Nationality
                         </x-input-label>
 
-                        <x-text-input class="w-full p-4 placeholder-gray-300" placeholder="Complex or Building Number "/>
+                        <x-select class="w-full p-4 placeholder-gray-300"/>
                     </div>
 
                     <div>
@@ -168,6 +225,51 @@
                     </div>
                 </div>
             </div>
+
+            <div class="bg-white flex p-8 flex-col rounded-[10px] mb-8">
+                <div class="pb-6 flex justify-between">
+                    <p class="font-medium text-base text-black">Personal Bank Account</p>
+
+                    <button class="flex text-gray-800 px-4 py-1 bg-nav-gray-50 rounded-full text-[14px] items-center">
+                        <x-svg.nigeria/>
+                        <span class="ml-2">
+                            NGN
+                        </span>
+                    </button>
+                </div>
+
+                <div>
+                    <div class="pb-4">
+                        <x-input-label class="pb-2">
+                            Bank Name
+                        </x-input-label>
+
+                        <x-input class="w-full p-4 placeholder-gray-300" placeholder="Enter Business Name"/>
+                    </div>
+
+                    <div class="pb-4">
+                        <x-input-label class="pb-2">
+                            Account Number
+                        </x-input-label>
+
+                        <x-input class="w-full p-4 placeholder-gray-300" placeholder="Enter Business Name"/>
+                    </div>
+
+                    <div class="pb-4">
+                        <x-input-label class="pb-2">
+                            Account Name
+                        </x-input-label>
+
+                        <x-input class="w-full p-4 placeholder-gray-300" placeholder="Enter Business Name"/>
+                    </div>
+
+                    <div>
+                        <button type="submit" class="text-white bg-primary-pink-500 rounded-md py-2 px-4 w-full">Save</button>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     </div>
 </x-dashboard-layout>
